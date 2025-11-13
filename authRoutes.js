1d39bef50e62ae6f7224f05ac860eeadfebe7e93
Lalit Patil <148002912+lalit2244@users.noreply.github.com>
2025-11-13 21:07:02 +0530

    Update authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -1,9 +1,10 @@
 // File: backend/routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('./authController');
 
 // POST /api/auth/login
 router.post('/login', authController.login);
+
 
 module.exports = router;
